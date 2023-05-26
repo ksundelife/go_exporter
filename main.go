a945998bde9057a1a1860caaca3ba12cca3234fd
Gritsyuk <ksundelife@yandex.ru>
2023-05-26 08:25:13 +0300

    [feature_1_2]: added new import

--- main.go
+++ main.go
@@ -2,10 +2,32 @@ package main
 
 import (
     "log"
+    "time"
     "net/http"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promauto"
     "github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+func recordMetrics() {
+    go func() {
+        for {
+            opsProcessed.Inc()
+            time.Sleep(2 * time.Second)
+        }
+    }()
+}
+
+var (
+    opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+        Name: "myapp_processed_ops_total",
+        Help: "The total number of processed events",
+    })
+)
+
 func main() {
+    recordMetrics()
+
     http.Handle("/metrics", promhttp.Handler())
     log.Fatal(http.ListenAndServe(":8082", nil))
 }
