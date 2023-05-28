edb3ee6d3e50ab247a02485fdf19a3b6a13d8043
Gritsyuk <ksundelife@yandex.ru>
2023-05-28 23:29:52 +0300

    [feature_1_3]: added goca

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module go_exporter
 go 1.18
 
 require (
+	github.com/OpenNebula/one/src/oca/go/src/goca v0.0.0-20230517101801-6d09265b614f // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kolo/xmlrpc v0.0.0-20190717152603-07c4ee3fd181 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/prometheus/client_golang v1.15.1 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
     "log"
+    "fmt"
+    "os"
+    "strconv"
     "time"
     "net/http"
+
     "github.com/prometheus/client_golang/prometheus"
     "github.com/prometheus/client_golang/prometheus/promauto"
     "github.com/prometheus/client_golang/prometheus/promhttp"
+    "github.com/OpenNebula/one/src/oca/go/src/goca"
 )
 
 func recordMetrics() {
@@ -30,4 +35,52 @@ func main() {
 
     http.Handle("/metrics", promhttp.Handler())
     log.Fatal(http.ListenAndServe(":8082", nil))
+        // Initialize connection with OpenNebula
+    con := map[string]string{
+        "user": "astra",
+        "password": "12345678",
+        "endpoint": "192.168.100.14",
+    }
+
+    client := goca.NewDefaultClient(
+        goca.NewConfig(con["user"], con["password"], con["endpoint"]),
+    )
+
+    controller := goca.NewController(client)
+
+    // Read VM ID from arguments
+    id, _ := strconv.Atoi(os.Args[1])
+
+    vmctrl := controller.VM(id)
+
+    // Fetch informations of the created VM
+    vm, err := vmctrl.Info(false)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("Shutting down %+v\n", vm.Name)
+
+    // Poweroff the VM
+    err = vmctrl.Poweroff()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Get short informations of the VMs
+    vms, err := controller.VMs().Info()
+    if err != nil {
+        log.Fatal(err)
+    }
+    for i := 0; i < len(vms.VMs); i++ {
+        // This Info method, per VM instance, give us detailed informations on the instance
+        // Check xsd files to see the difference
+        vm, err := controller.VM(vms.VMs[i].ID).Info(false)
+        if err != nil {
+            log.Fatal(err)
+        }
+        //Do some others stuffs on vm
+        fmt.Printf("%+v\n", vm)
+    }
 }
